c7df853723ef8a25bcf19c9b67ebba9e0265220c
Elias Rojas <eliasrojaslon@gmail.com>
2024-03-31 18:03:58 -0500

    CollapsingText component finished

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App.jsx";
+
+//import current component here, once finished, comment below code and uncomment upper App import
+// import App from "./components/CollapsingText.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
