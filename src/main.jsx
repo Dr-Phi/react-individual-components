bbd820f2cedc59302f1fa68635c899e249cabfd5
Elias Rojas <eliasrojaslon@gmail.com>
2024-04-07 15:54:14 -0500

    mobile first styles for split bill app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App.jsx";
+// import App from "./App.jsx";
 
 //import current component here, once finished, comment below code and uncomment upper App import
-// import App from "./components/CollapsingText.jsx";
+import App from "./components/SplitBill.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
