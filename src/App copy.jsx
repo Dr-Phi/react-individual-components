61ff09f8e8fa41d40a1aeaa36c2fbfc8650e152c
Elias Rojas <eliasrojaslon@gmail.com>
2024-04-07 12:59:04 -0500

    custom hooks geolocation starter files

---
+++ src/App copy.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+/* Here I will build my components from challenges,
+once finished, copy code and create new file and save it to the components folder,
+then move onto main.jsx and follow procedure to render and leave this App.jsx
+as it was originally. Happy hacking! ~Dr-Phi
+*/
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
