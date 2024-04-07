e573dd6605c9489394c81441dac6f9d93695ad13
Elias Rojas <eliasrojaslon@gmail.com>
2024-04-07 13:06:12 -0500

    custom hook useGeolocation created

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,15 @@
-import { useState } from "react";
+import "./App.css";
 
-function useGeolocation() {}
+/* Here I will build my components from challenges,
+once finished, copy code and create new file and save it to the components folder,
+then move onto main.jsx and follow procedure to render and leave this App.jsx
+as it was originally. Happy hacking! ~Dr-Phi
+*/
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [countClicks, setCountClicks] = useState(0);
-  const [position, setPosition] = useState({});
-  const [error, setError] = useState(null);
-
-  const { lat, lng } = position;
-
-  function getPosition() {
-    setCountClicks((count) => count + 1);
-
-    if (!navigator.geolocation)
-      return setError("Your browser does not support geolocation");
-
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-        setIsLoading(false);
-      },
-      (error) => {
-        setError(error.message);
-        setIsLoading(false);
-      }
-    );
-  }
-
   return (
     <div>
-      <button onClick={getPosition} disabled={isLoading}>
-        Get my position
-      </button>
-
-      {isLoading && <p>Loading position...</p>}
-      {error && <p>{error}</p>}
-      {!isLoading && !error && lat && lng && (
-        <p>
-          Your GPS position:{" "}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
-          >
-            {lat}, {lng}
-          </a>
-        </p>
-      )}
-
-      <p>You requested position {countClicks} times</p>
+      <h1>Hello World!</h1>
     </div>
   );
 }
